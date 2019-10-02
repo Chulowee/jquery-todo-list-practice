b4d493d37d7cda62a1fa714a460465c95e002013
Chloe Reyes <katrina.chloe.reyes@oocl.com>
2019-10-02 08:25:47 +0800

    Completed To Do List

--- js/todo.js
+++ js/todo.js
@@ -21,5 +21,35 @@ $(document)
             return uuid;
         }
 
+        $("#button").on('click', function(){
+            let input = $('input[name=ListItem]').val();
+
+         $("ol").append('<li id="'+ generateUUID() +'" class="notChecked"> ' +
+              '<input name="done-todo" type="checkbox" class="done-todo"><span contenteditable = true>' + input +'</span></li>');
+
+            input = $('input[name=ListItem]').val("");
+        });
+
+        $('ol').on('change','input[type=checkbox]', function() {
+            let item = $(this).closest('li');
+
+            $(item).toggleClass('checked notChecked')
+        });
+
+        $('#filters').on('click', "li a", function(){
+            $('ul').find('a').removeClass('selected');
+            $(this).addClass('selected');
+
+            let filter = $(this).data('filter');
+            let task = $('ol li');
+
+            $(task).show();
+            if(filter == 'active'){
+                $(task).not('.notChecked').hide();
+            } else if(filter == 'complete'){
+                $(task).not('.checked').hide();
+            }
+        });
+        
         // code to be implemented
     });
